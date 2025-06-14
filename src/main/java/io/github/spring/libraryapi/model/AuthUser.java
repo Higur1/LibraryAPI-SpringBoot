ab197c92855e0bf850e0084a736b8c47605b2343
Higur1 <higor.hungria466@gmail.com>
2025-06-13 22:58:55 -0300

    Create user authenticate
    add dependence hy-persistence
    columnDefinition
    
    //mapping List to Array
    @Type(ListArrayType.class)

---
+++ src/main/java/io/github/spring/libraryapi/model/AuthUser.java
@@ -0,0 +1,30 @@
+package io.github.spring.libraryapi.model;
+
+import io.hypersistence.utils.hibernate.type.array.ListArrayType;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.Type;
+
+import java.util.UUID;
+import java.util.List;
+
+
+@Entity
+@Table(name = "auth_user")
+@Data
+public class AuthUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Type(ListArrayType.class)
+    @Column(name = "roles", columnDefinition = "varchar[]")
+    private List<String> roles;
+}
+
