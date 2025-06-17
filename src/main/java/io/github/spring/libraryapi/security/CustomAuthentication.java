fdb05cb01ea4697301554f27da4e522e9a0c299c
Higur1 <higor.hungria466@gmail.com>
2025-06-16 21:55:19 -0300

    create CustomAuthentication to unify authentications

---
+++ src/main/java/io/github/spring/libraryapi/security/CustomAuthentication.java
@@ -0,0 +1,65 @@
+package io.github.spring.libraryapi.security;
+
+
+import io.github.spring.libraryapi.model.AuthUser;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import javax.security.auth.Subject;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Getter
+public class CustomAuthentication implements Authentication {
+
+    private final AuthUser authUser;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authUser
+                .getRoles()
+                .stream()
+                .map(role ->
+                        new SimpleGrantedAuthority(role)
+                ).collect(Collectors.toList());
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return authUser;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return authUser;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return true;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public String getName() {
+        return authUser.getLogin();
+    }
+
+    @Override
+    public boolean implies(Subject subject) {
+        return Authentication.super.implies(subject);
+    }
+}
