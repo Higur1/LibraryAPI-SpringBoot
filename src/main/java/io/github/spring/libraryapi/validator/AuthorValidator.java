ec9763ddc3a79066bb7ef56778258f727ba93840
Higur1 <higor.hungria466@gmail.com>
2025-06-26 00:17:25 -0300

    Fix update validator error

--- src/main/java/io/github/spring/libraryapi/validator/AuthorValidator.java
+++ src/main/java/io/github/spring/libraryapi/validator/AuthorValidator.java
@@ -6,30 +6,23 @@ import io.github.spring.libraryapi.repository.AuthorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 @Component
 @RequiredArgsConstructor
 public class AuthorValidator {
     private final AuthorRepository repository;
 
-    public void validate(Author author){
-        if(hasAuthor(author)){
-            throw  new DuplicateRecordException("Author already exists");
+    public void validate(Author author) {
+        if (hasAuthor(author)) {
+            throw new DuplicateRecordException("Author already exists");
         }
     }
 
-    private boolean hasAuthor(Author author){
-        Optional<Author> authorResultFind = repository.findByNameAndBirthDateAndNationality(
+    private boolean hasAuthor(Author author) {
+        return repository.findByNameAndBirthDateAndNationality(
                 author.getName(),
                 author.getBirthDate(),
                 author.getNationality()
-        );
-
-        if(author.getId() == null){
-            return authorResultFind.isPresent();
-        }
-
-        return !author.getId().equals(authorResultFind.get().getId()) && authorResultFind.isPresent();
+        ).map(found -> !author.getId().equals(found.getId())
+        ).orElse(false);
     }
 }
